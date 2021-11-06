fa60435c2eb566a96b68acbdb61c67aba87bd1a2
Walley Yang <walleyyang@gmail.com>
2021-11-05 21:41:20 -0400

    Updated server

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express, { Request, Response } from 'express';
 import 'dotenv/config';
 
 import { Flow, Alert } from 'common/models';
-// import { validFlow } from 'validators/flowValidator';
-// import { validAlert } from 'validators/alertValidator';
+import { modifyFlow } from 'modifiers/flowModifier';
+import { validFlow } from 'validators/flowValidator';
+import { validAlert } from 'validators/alertValidator';
+import { validFlowMessage } from 'validators/validFlowMessage';
 
 const createServer = () => {
   const port = process.env.DATA_HANDLER_PORT || '';
@@ -14,39 +16,32 @@ const createServer = () => {
   app.use(express.json());
 
   app.post('/flow', (req: Request) => {
-    const flow = req.body as Flow;
-    // modify
-    // filter
+    const modifiedFlow = modifyFlow(req.body as Flow);
 
-    // if (validFlow(flow)) {
-    //   // post to mongo
-    //   handleFlow(flow);
-    // }
+    if (validFlow(modifiedFlow)) {
+      // post to mongo
+      console.log('post flow to mongo...');
+      if (validFlowMessage(modifiedFlow)) {
+        console.log('send flow to datjuanitagurl');
+        console.log(modifiedFlow);
+      }
+    }
   });
 
   app.post('/alert', (req: Request) => {
     const alert = req.body as Alert;
-    console.log('in alert..');
-    console.log(alert);
-    // if (validAlert(alert)) {
-    //   // post to mongo
-    //   handleAlert(alert);
-    // }
+
+    if (validAlert(alert)) {
+      // post to mongo to keep track of flow
+
+      console.log('send alert to datjuanitagurl');
+      console.log(alert);
+    }
   });
 
   return app.listen(port, () => {
     console.log(`DJG Data Handler listening on port: ${port}`);
   });
-};
-
-const handleFlow = (flow: Flow) => {
-  console.log('handling flow....');
-  console.log(flow);
-};
-
-const handleAlert = (alert: Alert) => {
-  console.log('handling alert....');
-  console.log(alert);
 };
 
 export { createServer };
